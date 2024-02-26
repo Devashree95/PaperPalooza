7a3040c9e0dbaef16519f7e808089024612c8369
Vivek Murali <vivekmurali2k@gmail.com>
2024-02-26 12:42:17 -0500

    Basic UI

--- pages/login.py
+++ pages/login.py
@@ -1,43 +1,24 @@
 import streamlit as st
-from google_auth_oauthlib.flow import Flow
-from google.auth.transport.requests import Request
-import os
-from dotenv import load_dotenv
 
+def main():
+    st.title("Login Page")
 
-load_dotenv()
+    # Create placeholders for username and password input
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
 
-# Your Google OAuth 2.0 client ID and client secret
-CLIENT_ID = os.getenv('CLIENT_ID')
-CLIENT_SECRET = os.getenv('CLIENT_SECRET')
-SCOPES = ["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email"]
-REDIRECT_URI = "http://localhost:8501/login"
+    # Check if the login button is clicked
+    if st.button("Login"):
+       login()
+    
+    if st.button("Sign Up"):
+        signup()
 
-st.subheader("Sign in with Google")
+def login():
+    pass
 
-# Create the authorization flow
-flow = Flow.from_client_secrets_file(
-    "client_secret.json", scopes=SCOPES, redirect_uri=REDIRECT_URI)
+def signup():
+    pass
 
-# Generate the authorization URL
-authorization_url, _ = flow.authorization_url(
-    access_type='offline', include_granted_scopes='true')
-
-query_params = st.experimental_get_query_params()
-code = query_params.get('code')
-
-if code:
-    flow.fetch_token(code=code)
-
-    # Get user info
-    user_info = flow.credentials.id_token
-    st.write("User Info:", user_info)
-
-# Display the login button
-if st.button("Sign in with Google"):
-    # Redirect to Google's login page
-    st.markdown(f"[Sign in with Google]({authorization_url})")
-
-    # Handle the callback after successful authentication
-
-
+if __name__ == "__main__":
+    main()
