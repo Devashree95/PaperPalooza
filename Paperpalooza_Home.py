be5aa809cd4eaf86841dd5474e67b6fe6c1234d8
Devashree95 <devashree.bhagwat95@gmail.com>
2024-03-18 14:19:20 -0400

    adds basic login page

--- Paperpalooza_Home.py
+++ Paperpalooza_Home.py
@@ -10,9 +10,21 @@ st.set_page_config(
 	page_icon="📄"
 )
 
+st.markdown("""
+    <style>
+        section[data-testid="stSidebar"][aria-expanded="true"]{
+            display: none;
+        }
+    </style>
+    """, unsafe_allow_html=True)
+
+col3, col1, col2 = st.columns([1,2, 1])
+
 logo = "./images/profile_3135715.png"
 image = Image.open(logo)
 
+logo1 = "./images/paperpalooza.png"
+image = Image.open(logo1)
 
 # Function to convert image to Base64
 def get_image_as_base64(path):
@@ -40,50 +52,95 @@ def set_background_from_local_file(path):
     """
     st.markdown(css, unsafe_allow_html=True)
     
-set_background_from_local_file('./images/background.png')
+set_background_from_local_file('./images/loginbkg.png')
 
-st.markdown(f"""
-			<a href="/" style="color:white;text-decoration: none;">
-				<div style="display:table;margin-top:-15 rem;margin-left:0%; display: flex;">
-			  		<img src="{image_base64}" alt="PaperPalooza Logo" style="width:50px;height:40px;margin-left:750px; flex:2;" </img>
-					<span style="padding:10px; flex:2;">Username</span>
-				</div>
-			</a>
-			<br>
-				""", unsafe_allow_html=True)
+main_content = st.empty()
 
-# selected2 = option_menu(None,	 ["My Profile"], 
-#     icons=['gear'], 
-#     menu_icon="cast", default_index=0, orientation="horizontal")
+users = {
+    "user1": "password1",
+    "user2": "password2"
+}
 
+def authenticate_user(username, password):
+    if username in users and users[username] == password:
+        return True
+    return False
 
-helpers.sidebar.show()
+def show_login_page():
+    with col2:
+          image_base64 = get_image_as_base64(logo1)
+          st.markdown(f"""
+				<a href="/" style="color:white;text-decoration: none;">
+					<div style="display:table;margin-top:-15 rem;margin-left:0%; display: flex;">
+						<img src="{image_base64}" alt="Insurehub Logo" style="width:100px;height:100px;" </img>
+					</div>
+				</a>
+				<br>
+					""", unsafe_allow_html=True)
+    with col1:
+          st.title("Welcome to Paperpalooza!")
+          st.subheader("Login")
+          username = st.text_input("Username")
+          password = st.text_input("Password", type="password")
+          if st.button("Login"):
+            if authenticate_user(username, password):
+                st.session_state["authenticated"] = True
+				#st.success("You're logged in!")
+                st.experimental_rerun()
+            else:
+                st.error("Invalid username or password")
 
-st.header("Welcome to Paperpalooza!")
-
-st.subheader("Latest Research at Virginia Tech:")
-
-video1_url = 'https://www.youtube.com/watch?v=5WyKVUtw90g'
-video2_url = 'https://www.youtube.com/watch?v=ehn0aSEUJVM'
-video3_url = 'https://www.youtube.com/watch?v=tAyQ9sUfPIo'
-video4_url = 'https://www.youtube.com/watch?v=fCUqJeLl2to'
-
-col1, col2 = st.columns(2)
-
-with col1:
-    st.video(video1_url)
-    st.video(video3_url)
+def show_homepage():
+    st.markdown("""
+    <style>
+        section[data-testid="stSidebar"][aria-expanded="true"]{
+            display: block;
+        }
+    </style>
+    """, unsafe_allow_html=True)
+    st.markdown(f"""
+				<a href="/" style="color:white;text-decoration: none;">
+					<div style="display:table;margin-top:-15 rem;margin-left:0%; display: flex;">
+						<img src="{image_base64}" alt="PaperPalooza Logo" style="width:50px;height:40px;margin-left:750px; flex:2;" </img>
+						<span style="padding:10px; flex:2;">Username</span>
+					</div>
+				</a>
+				<br>
+					""", unsafe_allow_html=True)
+    helpers.sidebar.show()
+    st.header("Welcome to Paperpalooza!")
+    st.subheader("Latest Research at Virginia Tech:")
+    video1_url = 'https://www.youtube.com/watch?v=5WyKVUtw90g'
+    video2_url = 'https://www.youtube.com/watch?v=ehn0aSEUJVM'
+    video3_url = 'https://www.youtube.com/watch?v=tAyQ9sUfPIo'
+    video4_url = 'https://www.youtube.com/watch?v=fCUqJeLl2to'
+    col1, col2 = st.columns(2)
+    with col1:
+          st.video(video1_url)
+          st.video(video3_url)
+          
+    with col2:
+          st.video(video2_url)
+          st.video(video4_url)
+    st.subheader("Resources:")
+    st.markdown("[Virginia Tech Research Resources](https://www.research.vt.edu/research-development/resources.html)")
+    st.markdown("[Research Support](https://lib.vt.edu/research-teaching/research-services.html)")
+    st.markdown("[Virginia Tech Research](https://www.research.vt.edu/)")
     
-with col2:
-    st.video(video2_url)
-    st.video(video4_url)
+if "authenticated" not in st.session_state:
+    st.session_state["authenticated"] = False
     
-st.subheader("Resources:")
-st.markdown("[Virginia Tech Research Resources](https://www.research.vt.edu/research-development/resources.html)")
 
-st.markdown("[Research Support](https://lib.vt.edu/research-teaching/research-services.html)")
+if not st.session_state["authenticated"]:
+    with main_content.container():
+        # Function to show the login page
+        show_login_page()  # This function will include login form and authentication logic
+else:
+    # Clear the main content placeholder now that the user is authenticated
+    main_content.empty()
+    show_homepage()
 
-st.markdown("[Virginia Tech Research](https://www.research.vt.edu/)")
+
 
 
     
